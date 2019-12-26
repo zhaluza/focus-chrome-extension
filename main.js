7f2e57ed143eab4ce2c6a13aaff9d65c491a74a7
Zac H <zac.haluza@gmail.com>
2019-12-25 22:52:26 -0500

    now displays random images

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-// Make a single div with both elements inside
+function getRandomNum(min, max) {
+  var result = Math.random() * (max - min + min);
+  return Math.floor(result);
+}
 
 // YouTube
 var ytContent = $('#content');
 var ytRemove = $('#page-manager');
 if (ytContent) {
   ytRemove.remove();
-  ytContent.append(
-    '<div class="center-div"><p class="center text">Get to work!</p><img class="cat-pic" src="https://images.unsplash.com/photo-1482066490729-6f26115b60dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" alt="an adorable cat" /></div>'
-  );
+  $.get('https://unsplash.it/list', function(result) {
+    var randomNum = getRandomNum(0, result.length);
+    var randomId = result[randomNum].id;
+    var randomImg = `https://unsplash.it/1200/800?image=${randomId}`;
+    ytContent.append(
+      `<div class="center-div"><p class="center text">Get to work!</p><img class="random-pic" src=${randomImg} alt="random image!" /></div>`
+    );
+  });
 }
 
 // Facebook
@@ -15,19 +23,29 @@ var fbContent = $('#contentArea');
 var fbParent = fbContent.parent();
 var fbAds = $('#pagelet_ego_pane');
 if (fbContent) {
-  fbParent.append(
-    '<div class="center-div"><p class="center text">Get to work!</p><img class="cat-pic" src="https://images.unsplash.com/photo-1482066490729-6f26115b60dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" alt="an adorable cat" /></div>'
-  );
   fbContent.remove();
   fbAds.remove();
+  $.get('https://unsplash.it/list', function(result) {
+    var randomNum = getRandomNum(0, result.length);
+    var randomId = result[randomNum].id;
+    var randomImg = `https://unsplash.it/1200/800?image=${randomId}`;
+    fbParent.append(
+      `<div class="center-div"><p class="center text">Get to work!</p><img class="random-pic" src=${randomImg} alt="random image!" /></div>`
+    );
+  });
 }
 
 // Reddit
 var rdContent = $('#2x-container');
 var rdParent = rdContent.parent();
 if (rdContent) {
-  rdParent.append(
-    '<div class="center-div"><p class="center text">Get to work!</p><img class="cat-pic" src="https://images.unsplash.com/photo-1482066490729-6f26115b60dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" alt="an adorable cat" /></div>'
-  );
   rdContent.remove();
+  $.get('https://unsplash.it/list', function(result) {
+    var randomNum = getRandomNum(0, result.length);
+    var randomId = result[randomNum].id;
+    var randomImg = `https://unsplash.it/1200/800?image=${randomId}`;
+    rdParent.append(
+      `<div class="center-div"><p class="center text">Get to work!</p><img class="random-pic" src=${randomImg} alt="random image!" /></div>`
+    );
+  });
 }
